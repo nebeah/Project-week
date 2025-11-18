b1478b82300cb29560698701ba2dc9257109ca89
nebeah <mo.nabyh@gmail.com>
2025-11-19 00:11:36 +0800

    Tidied up

--- Week-two/Week-two.ino
+++ Week-two/Week-two.ino
@@ -1,6 +1,6 @@
 #include<LiquidCrystal.h>
 
-// Digital pins 10 to 4 are occupied by LCD display, 10 if for turning on backlight
+// Digital pins 10 to 4 are occupied by LCD display, 10 is for turning on backlight
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 // Motor driver pins - pins 3 & 11 are PWM pins
@@ -11,69 +11,70 @@ int in2 = A2;
 int in3 = A3;
 int in4 = A4;
 
-// Encoder pins
 int rightEncoder = 2;
 int leftEncoder = 1;
 
 int rightSteps = 0;
 int leftSteps = 0;
 struct State {
-  bool right = false;
-  bool left = false;
+    bool right = false;
+    bool left = false;
 } prevState, currentState;
 
 void setup() {
-  pinMode(enA, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+    pinMode(enA, OUTPUT);
+    pinMode(enB, OUTPUT);
+    pinMode(in1, OUTPUT);
+    pinMode(in2, OUTPUT);
+    pinMode(in3, OUTPUT);
+    pinMode(in4, OUTPUT);
 
-  pinMode(rightEncoder, INPUT);
-  pinMode(leftEncoder, INPUT);
+    pinMode(rightEncoder, INPUT);
+    pinMode(leftEncoder, INPUT);
 
-  analogWrite(enA, 255);
-  analogWrite(enB, 255);
+    analogWrite(enA, 255);
+    analogWrite(enB, 255);
 
-  moveForward();
+    moveForward();
 
-  lcd.begin(16, 2);
-  lcd.setCursor(0, 0);
-  lcd.print("Right: ");
-  lcd.setCursor(7, 0);
-  lcd.print(rightSteps);
+    lcd.begin(16, 2);
+    lcd.setCursor(0, 0);
+    lcd.print("Right: ");
+    lcd.setCursor(7, 0);
+    lcd.print(rightSteps);
 
-  lcd.setCursor(0, 1);
-  lcd.print("Left: ");
-  lcd.setCursor(6, 1);
-  lcd.print(leftSteps);
+    lcd.setCursor(0, 1);
+    lcd.print("Left: ");
+    lcd.setCursor(6, 1);
+    lcd.print(leftSteps);
 
-  currentState.right = digitalRead(rightEncoder);
-  currentState.left = digitalRead(leftEncoder);
-  prevState = currentState;
+    currentState.right = digitalRead(rightEncoder);
+    currentState.left = digitalRead(leftEncoder);
+    prevState = currentState;
 }
 
 void loop() {
-  currentState.right = digitalRead(rightEncoder);
-  currentState.left = digitalRead(leftEncoder);
-  if (currentState.right && !prevState.right) {
-    rightSteps++;
-    lcd.setCursor(7, 0);
-    lcd.print(rightSteps);
-  }
-  if (currentState.left && !prevState.left) {
-    leftSteps++;
-    lcd.setCursor(6, 1);
-    lcd.print(leftSteps);
-  }
+    currentState.right = digitalRead(rightEncoder);
+    currentState.left = digitalRead(leftEncoder);
 
-  prevState = currentState;
+    if (currentState.right && !prevState.right) {
+        rightSteps++;
+        lcd.setCursor(7, 0);
+        lcd.print(rightSteps);
+    }
+    
+    if (currentState.left && !prevState.left) {
+        leftSteps++;
+        lcd.setCursor(6, 1);
+        lcd.print(leftSteps);
+    }
+
+    prevState = currentState;
 }
 
 void moveForward() {
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
 }
