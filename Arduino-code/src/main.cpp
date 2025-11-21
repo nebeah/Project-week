7b573391457e6b3130a7e0765f0d59fc7fba5803
nebeah <mo.nabyh@gmail.com>
2025-11-22 00:09:20 +0800

    Updated logbook and added the working arduino code

--- Arduino-code/src/main.cpp
+++ Arduino-code/src/main.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // Digital pins 10 to 4 are occupied by LCD display, pin 10 is for turning on backlight
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 namespace Pins {
     namespace MotorDriver {
-        constexpr int enA = 3; // right side motors
+        constexpr int enA = 3; // Right side motors 
         constexpr int in1 = 1;
         constexpr int in2 = 2;
 
-        constexpr int enB = 11; // left side motors
+        constexpr int enB = 11; // Left side motors
         constexpr int in3 = 12;
         constexpr int in4 = 13;
     }
@@ -22,43 +22,63 @@ namespace Pins {
     namespace IR {
         constexpr int right = A1;
         constexpr int left = A2;
-        constexpr int center = A5;
     }
 }
 
-constexpr int defaultSpeed = 100;
-constexpr int maxSpeed = defaultSpeed * 2;
-constexpr int minSpeed = defaultSpeed * 0.6;
+constexpr int forwardSpeed = 85; 
+constexpr int turnSpeedLeft = 140; 
+constexpr int turnSpeedRight = 160; 
 
-// The analog threshold above which the IR sensors can be considered to be ON
-constexpr int IRthreshold = 100;
+// IR sensor thresholds for analogRead
+constexpr int leftThreshold = 300;
+constexpr int rightThreshold = 600;
+
+unsigned long startTime;
+unsigned long finalTime = 0;
+bool finished = false; 
 
 struct Encoder {
     int steps = 0;
     bool prevState = false;
-    float distance = 0;
 } leftEncoder, rightEncoder;
-constexpr float wheelCircumference = 0.207; // in metres
+
+constexpr float wheelCircumference = 0.207; // in meters
 constexpr int slits = 20;
 
-void updateDistance() {
+
+void updateEncoders() {
+    if (finished) return;
+
     bool leftState = digitalRead(Pins::Encoders::left);
     bool rightState = digitalRead(Pins::Encoders::right);
 
     if (leftState && !leftEncoder.prevState) leftEncoder.steps++;
     if (rightState && !rightEncoder.prevState) rightEncoder.steps++;
 
-    float avgSteps = (leftEncoder.steps + rightEncoder.steps) / 2;
-    float rotations = avgSteps / slits;
-    float distanceTravelled = rotations * wheelCircumference;
-
-    lcd.setCursor(0, 0);
-    lcd.print("Dist: ");
-    lcd.print(distanceTravelled);
-    lcd.print("m");
-
     leftEncoder.prevState = leftState;
     rightEncoder.prevState = rightState;
+}
+
+void updateLCD() {
+    // Calculate distance
+    float avgSteps = (leftEncoder.steps + rightEncoder.steps) / 2.0;
+    float distanceTravelled = (avgSteps / slits) * wheelCircumference; // number of rotations mulitplied by circumference
+
+    // Calculate time
+    unsigned long timer = finished ? finalTime : (millis() - startTime);
+    float timeInSeconds = timer / 1000.0;
+
+    // Row 0: Distance
+    lcd.setCursor(0, 0);
+    lcd.print("Dist: ");
+    lcd.print(distanceTravelled, 2); // 2 decimal places
+    lcd.print("m ");
+
+    // Row 1: Time
+    lcd.setCursor(0, 1);
+    lcd.print("Time: ");
+    lcd.print(timeInSeconds, 2); // 2 decimal places
+    lcd.print("s   "); // Extra spaces to clear any old artifacts
 }
 
 void setSpeed(int rightSpeed, int leftSpeed) {
@@ -67,54 +87,61 @@ void setSpeed(int rightSpeed, int leftSpeed) {
 }
 
 void turnRight() {
-    digitalWrite(Pins::MotorDriver::in1, LOW); // right motor go bacwards
+    digitalWrite(Pins::MotorDriver::in1, LOW); // right motors turns backwards
     digitalWrite(Pins::MotorDriver::in2, HIGH);
-
-    digitalWrite(Pins::MotorDriver::in3, HIGH); //left motor go backwards
+    digitalWrite(Pins::MotorDriver::in3, HIGH); // left motors turn forwards
     digitalWrite(Pins::MotorDriver::in4, LOW);
-
-    setSpeed(defaultSpeed, maxSpeed);
+    setSpeed(turnSpeedRight, turnSpeedRight);
 }
 
 void turnLeft() {
-    digitalWrite(Pins::MotorDriver::in1, HIGH); // right motor go forwards
+    digitalWrite(Pins::MotorDriver::in1, HIGH); // right motors turn forward
     digitalWrite(Pins::MotorDriver::in2, LOW);
-
-    digitalWrite(Pins::MotorDriver::in3, LOW); //left motor go backwards
+    digitalWrite(Pins::MotorDriver::in3, LOW); // left motors turn backwards
     digitalWrite(Pins::MotorDriver::in4, HIGH);
-
-    setSpeed(maxSpeed, defaultSpeed);
+    setSpeed(turnSpeedLeft, turnSpeedLeft);
 }
 
-bool rotating = false;
-enum Turning {
-    left,
-    right
-};
+void moveForward() {
+    digitalWrite(Pins::MotorDriver::in1, HIGH); // right motors turn forwards
+    digitalWrite(Pins::MotorDriver::in2, LOW);
+    digitalWrite(Pins::MotorDriver::in3, HIGH); // left motors turn forwards
+    digitalWrite(Pins::MotorDriver::in4, LOW);
+    setSpeed(forwardSpeed, forwardSpeed);
+}
 
-Turning turning = left;
+void stopMotors() {
+    setSpeed(0, 0);
+    digitalWrite(Pins::MotorDriver::in1, LOW);
+    digitalWrite(Pins::MotorDriver::in2, LOW);
+    digitalWrite(Pins::MotorDriver::in3, LOW);
+    digitalWrite(Pins::MotorDriver::in4, LOW);
+}
 
 void followLine() {
-    // Check if the the sensors see a line
-    bool rightIR = analogRead(Pins::IR::right) < IRthreshold;
-    bool leftIR = analogRead(Pins::IR::left) < IRthreshold;
-
-    if (turning == left) {
-        if (rightIR) {
-            turning = right;
-            turnRight();
-        }
-    } else if (turning == right) {
-        if (leftIR) {
-            turning = left;
-            turnLeft();
-        }
+    if (finished) {
+        stopMotors();
+        updateLCD(); 
+        return;
     }
+
+    // check if the sensors see a line
+    bool rightOnLine = (analogRead(Pins::IR::right) < rightThreshold);
+    bool leftOnLine = (analogRead(Pins::IR::left) < leftThreshold);
+
+    if (rightOnLine && leftOnLine) {
+        finished = true;
+        finalTime = millis() - startTime;
+        stopMotors();
+        updateLCD();
+        return;
+    }
+    else if (rightOnLine) { turnRight(); }
+    else if (leftOnLine) { turnLeft(); }
+    else { moveForward(); }
 }
 
 void setup() {
-    Serial.begin(9600);
-
     pinMode(Pins::MotorDriver::enA, OUTPUT);
     pinMode(Pins::MotorDriver::enB, OUTPUT);
     pinMode(Pins::MotorDriver::in1, OUTPUT);
@@ -128,9 +155,21 @@ void setup() {
     pinMode(Pins::IR::left, INPUT);
 
     lcd.begin(16, 2);
+    lcd.print("Ready...");
+    delay(1000);
+    lcd.clear();
+    
+    startTime = millis();
 }
 
+unsigned long lastLCDUpdate = 0;
+
 void loop() {
-    updateDistance();
+    updateEncoders();
     followLine();
+
+    if (millis() - lastLCDUpdate > 250) {
+        updateLCD();
+        lastLCDUpdate = millis();
+    }
 }
